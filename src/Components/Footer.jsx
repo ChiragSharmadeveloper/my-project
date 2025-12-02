1dba7bbd6f1610bf02bb0c6a9652f728aeec1367
ChiragShamradeveloper <chiragsharmadeveloper@gmail.com>
2025-12-02 15:51:53 +0530

    modify

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,8 +5,7 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <>
-       {/* FOOTER */}
-
+           {/* FOOTER */}
       <footer className="bg-black text-white p-8 mt-20">
         <div className="max-w-7xl mx-auto px-4 text-center"
          onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}>
@@ -42,6 +41,7 @@ const Footer = () => {
           <p className='mt-16 text-lg font-semibold'>® 2022 — All Rights Reserved</p>
           <p className=' text-lg font-semibold'>Email: babitasports5@gmail.com | Phone: +91 9997270808 </p>
           <p className='text-4xl font-semibold'>© BABITA BHARDWAJ SPORTS </p></Link>
+       
         </div>
       </footer>
       
